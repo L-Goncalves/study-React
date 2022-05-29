acf33070bd0636aeb8606c2a478bf90d439010f8
Lucas Gonçalves <ldgoncalvs@gmail.com>
2022-05-29 18:10:08 -0300

    add: logo, input search field

--- restaurant-search/src/pages/Home/index.jsx
+++ restaurant-search/src/pages/Home/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import {Container} from './styles'
+import React, { useState } from "react";
+import { Container, Search } from "./styles";
+import logo from "../../assets/logo.svg";
+import TextField, { Input } from "@material/react-text-field";
 const Home = () => {
-    return (
+  const [searchValue, setSearchValue] = useState("");
+  return (
     <>
-        <Container>Hello World!</Container>
-    </>)
-}
-
+      <Container>
+        <Search>
+          <img src={logo} alt="Restaurant Logo" />
+        </Search>
+        <TextField label={"Pesquisar"} outlined>
+          <Input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </TextField>
+      </Container>
+    </>
+  );
+};
 
 export default Home;
