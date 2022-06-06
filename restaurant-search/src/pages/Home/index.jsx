569afc5868aa67ea2c5a9a592a68901ff906a80d
Lucas Gonçalves <ldgoncalvs@gmail.com>
2022-06-06 00:52:20 -0300

    fix: GoogleMap was undefined

--- restaurant-search/src/pages/Home/index.jsx
+++ restaurant-search/src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, SearchInput, RestaurantLogo, Wrapper, Map, CarouselTitle} from "./styles";
+import { Container, SearchInput, RestaurantLogo, Wrapper, GoogleMap, CarouselTitle} from "./styles";
 import logo from "../../assets/logo.svg";
 import TextField, { Input } from "@material/react-text-field";
 import MaterialIcon from "@material/react-material-icon";
