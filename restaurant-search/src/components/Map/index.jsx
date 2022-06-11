aaf1eaf4661b426166e00d4c9edfb670cd8ecc56
Lucas Gonçalves <ldgoncalvs@gmail.com>
2022-06-11 18:42:50 -0300

    add: adding axios to use with tripAdvisor api

--- restaurant-search/src/components/Map/index.jsx
+++ restaurant-search/src/components/Map/index.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
 
 export const MapContainer = (props) => {
   const { google } = props;
+  const [map, setMap] = useState(null)
 
-  return <Map google={google} centerAroundCurrentLocation={true}></Map>;
+  const searchNearby = (map) => {
+    const service = new google.maps.places.PlacesService(map);
+
+    const request = {
+        location: map.center,
+        radius: '20000',
+        type: ['restaurant']
+    }
+
+    service.nearbySearch(request, (results, status) => {
+        if(status === google.maps.places.PlacesService.OK){
+            console.log('restaurantes:', results)
+        }
+    })
+
+  }
+
+  const onMapReady = (_, map) => {
+     setMap(map)
+     searchNearby(map)
+  }
+  return (
+  <Map google={google} centerAroundCurrentLocation={true}
+  onReady={onMapReady}
+  onRecenter={onMapReady}
+  />);
 };
 
 export default GoogleApiWrapper({
