8386eff2e078d65266cfcf6991df088dfcf78101
Lucas Gonçalves <ldgoncalvs@gmail.com>
2022-06-20 02:52:09 -0300

    chore: developing Details Screen

--- nftapp/components/NFTCard.js
+++ nftapp/components/NFTCard.js
@@ -1,10 +1,9 @@
 import {View, Text, Image} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 import { CircleButton } from '../components';
 import { COLORS, SIZES, SHADOWS, assets} from '../constants';
 import { SubInfo, EthPrice, NFTTitle } from './SubInfo';
 import {RectButton} from './Button'
-const NFTCard = ({data}) => {
+const NFTCard = ({data , navigation }) => {
     return(
         <View style={{
             backgroundColor: COLORS.white, 
--- nftapp/screens/Details.js
+++ nftapp/screens/Details.js
@@ -1,6 +1,53 @@
-const Details = () => {
-    return (<Text>Details</Text>)
-}
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  FlatList,
+} from "react-native";
 
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from "../constants";
+import {
+  CircleButton,
+  RectButton,
+  SubInfo,
+  FocusedStatusBar,
+  DetailsDesc,
+  DetailsBid,
+} from "../components";
 
-export default Details
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255, 255, 255, 0.5",
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+
+      <FlatList
+        data={data.bids}
+        renderItem={({item}) => <DetailsBid bid={item}/>}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Details;
