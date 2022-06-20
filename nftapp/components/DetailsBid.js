1945c58108a98fa6960f56ce2e66cc0cad1169ae
Lucas Gonçalves <ldgoncalvs@gmail.com>
2022-06-20 02:50:28 -0300

    add: new components

---
+++ nftapp/components/DetailsBid.js
@@ -0,0 +1,8 @@
+import {View, Text, SafeAreaView, Image, StatusBar, FlatList} from 'react-native';
+
+const DetailsBid = () => {
+    return (<Text>Details</Text>)
+}
+
+
+export default DetailsBid;
---
+++ nftapp/components/DetailsDesc.js
@@ -0,0 +1,9 @@
+import {View, Text, SafeAreaView, Image, StatusBar, FlatList} from 'react-native';
+
+
+const DetailsDesc = () => {
+    return (<Text>Details</Text>)
+}
+
+
+export default DetailsDesc
